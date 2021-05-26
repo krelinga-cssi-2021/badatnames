27567b99522e8c8eaf58cd4ca83a5fa82bc88a37
Andy Kreling <krelinga.cssi.2021@gmail.com>
2021-05-26 02:37:48 +0000

    a sign-in button that can detect clicks

--- public/index.js
+++ public/index.js
@@ -1,1 +1,6 @@
-alert("Got here!")
+document.addEventListener("DOMContentLoaded", () => {
+    const signInButton = document.querySelector("button")
+    signInButton.addEventListener("click", () => {
+        alert("clicked!")
+    })
+})
