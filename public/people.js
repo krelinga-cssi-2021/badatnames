42569609b102d296eb8e9738aaa58120ad74eb01
Andy Kreling <krelinga.cssi.2021@gmail.com>
2021-05-26 03:46:30 +0000

    Use some better placeholder images.

--- public/people.js
+++ public/people.js
@@ -3,7 +3,7 @@ function newCardElement(personInfo) {
         <div class="card">
             <div class="card-image">
                 <figure class="image is-4by3">
-                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                    <img src="${personInfo.imgUrl}" alt="Placeholder image">
                 </figure>
             </div>
             <div class="card-content">
@@ -21,7 +21,7 @@ function newCardElement(personInfo) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector("#first").appendChild(newCardElement({name: "Andy"}))
-    document.querySelector("#second").appendChild(newCardElement({name: "Suzy"}))
-    document.querySelector("#third").appendChild(newCardElement({name: "Hygge"}))
+    document.querySelector("#first").appendChild(newCardElement({name: "Andy", imgUrl: "https://c402277.ssl.cf1.rackcdn.com/photos/6526/images/hero_full/sloth_%28c%29_Jorge_Salas_International_Expeditions.JPG?1394634201"}))
+    document.querySelector("#second").appendChild(newCardElement({name: "Suzy", imgUrl: "https://www.prestigeanimalhospital.com/sites/default/files/puppy-dental-care.jpg"}))
+    document.querySelector("#third").appendChild(newCardElement({name: "Hygge", imgUrl: "https://icatcare.org/app/uploads/2019/09/The-Kitten-Checklist-1.png"}))
 })
