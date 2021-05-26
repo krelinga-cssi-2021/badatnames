f8b8d60ac82ed06edc5c2f9e9690046e28a60443
Andy Kreling <krelinga.cssi.2021@gmail.com>
2021-05-26 02:48:50 +0000

    Basic log-in prompt working

--- public/index.js
+++ public/index.js
@@ -1,6 +1,35 @@
+function signIn() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+            /** @type {firebase.auth.OAuthCredential} */
+            var credential = result.credential;
+            var token = credential.accessToken;
+
+            // The signed-in user info.
+            var user = result.user;
+            //window.location = 'writeNote.html';
+            alert("signed in!")
+        }).catch((error) => {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            const err = {
+                errorCode,
+                errorMessage,
+                email,
+                credential
+            };
+            console.log(err);
+        })            
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const signInButton = document.querySelector("button")
-    signInButton.addEventListener("click", () => {
-        alert("clicked!")
-    })
+    signInButton.addEventListener("click", signIn)
 })
